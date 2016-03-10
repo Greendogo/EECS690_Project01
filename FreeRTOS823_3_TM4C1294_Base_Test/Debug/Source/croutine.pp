3631bda772393d90bc09b049402b9d40e105d864
unknown <pdoddapa@3002-2.eecs.ku.edu>
2016-03-10 16:11:44 -0600

    Initial commit.

---
+++ FreeRTOS823_3_TM4C1294_Base_Test/Debug/Source/croutine.pp
@@ -0,0 +1,53 @@
+# FIXED
+
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/croutine.c
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/FreeRTOS.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/portable/CCS/ARM_CM4F/portmacro.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/FreeRTOS823_3_TM4C1294_Base_Test/FreeRTOSConfig.h
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/FreeRTOS823_3_TM4C1294_Base_Test/Drivers/Processor_Initialization.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h
+Source/croutine.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/projdefs.h
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/portable.h
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/deprecated_definitions.h
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/mpu_wrappers.h
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/task.h
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/list.h
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/croutine.h
+Source/croutine.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/list.h
+
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/croutine.c: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/FreeRTOS.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/portable/CCS/ARM_CM4F/portmacro.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/FreeRTOS823_3_TM4C1294_Base_Test/FreeRTOSConfig.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/FreeRTOS823_3_TM4C1294_Base_Test/Drivers/Processor_Initialization.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/projdefs.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/portable.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/deprecated_definitions.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/mpu_wrappers.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/task.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/list.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/croutine.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/list.h: 
---
+++ FreeRTOS823_3_TM4C1294_Base_Test/Debug/Source/event_groups.pp
@@ -0,0 +1,55 @@
+# FIXED
+
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/event_groups.c
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdlib.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/FreeRTOS.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/portable/CCS/ARM_CM4F/portmacro.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/FreeRTOS823_3_TM4C1294_Base_Test/FreeRTOSConfig.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/FreeRTOS823_3_TM4C1294_Base_Test/Drivers/Processor_Initialization.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h
+Source/event_groups.obj: C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/projdefs.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/portable.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/deprecated_definitions.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/mpu_wrappers.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/task.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/list.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/timers.h
+Source/event_groups.obj: C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/event_groups.h
+
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/event_groups.c: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdlib.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/FreeRTOS.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/portable/CCS/ARM_CM4F/portmacro.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/FreeRTOS823_3_TM4C1294_Base_Test/FreeRTOSConfig.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/FreeRTOS823_3_TM4C1294_Base_Test/Drivers/Processor_Initialization.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stddef.h: 
+C:/TI_CodeComposer/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/projdefs.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/portable.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/deprecated_definitions.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/mpu_wrappers.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/task.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/list.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/timers.h: 
+C:/Projects/TI_ARM_Tiva_Projects_EECS_690_Sp16/Source/include/event_groups.h: 
